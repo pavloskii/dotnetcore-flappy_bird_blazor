f11019577f5dc091b10a6821784908ce11994f2c
pavloskii <pavlo.igor@gmail.com>
2020-08-21 23:41:37 +0200

    part 1

---
+++ FlappyBirdBlazor.Web/Models/BirdModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FlappyBirdBlazor.Web.Models
+{
+    public class BirdModel
+    {
+        public int DistanceFromGround { get; set; } = 100;
+
+        public void Fall(int gravity)
+        {
+            DistanceFromGround -= gravity;
+        }
+    }
+}
---
+++ FlappyBirdBlazor.Web/Models/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace FlappyBirdBlazor.Web.Models
+{
+    public class GameManager: INotifyPropertyChanged
+    {
+        private readonly int _gravity = 2;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public BirdModel Bird { get; set; }
+        public bool IsRunning { get; set; } = false;
+
+        public GameManager()
+        {
+            Bird = new BirdModel();
+        }
+
+        public async void MainLoop()
+        {
+            IsRunning = true;
+
+            while (IsRunning)
+            {
+                Bird.Fall(_gravity);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Bird)));
+
+                if (Bird.DistanceFromGround <= 0)
+                    GameOver();
+
+                await Task.Delay(20);
+            }
+        }
+
+        public void StartGame()
+        {
+            if (!IsRunning)
+            {
+                Bird = new BirdModel();
+                MainLoop();
+            }
+        }
+
+        public void GameOver()
+        {
+            IsRunning = false;
+        }
+    }
+}
